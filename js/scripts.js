ec07a58b79ab6528216cf46d604521108c54ab4b
Joe Kramer and Timberlake <timberlake@>
2017-06-14 14:50:27 -0700

    get spaces to only be able to set once, and record space value

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 function Player(turn) {
-  type = "X";
+  this.type = "X";
   this.turn = turn;
 }
 
 function Space(row, column) {
-  type = "";
-  empty = true;
+  this.type = "";
+  this.empty = true;
   this.row = row;
   this.column = column;
 }
 
 function Board() {
-  var counter = 0;
-  var spaces = [];
+  this.counter = 0;
+  this.spaces = [];
 }
 
 Board.prototype.initializeSpaces = function() {
@@ -53,102 +53,138 @@ $(document).ready(function(){
   }
 
   $(".space1").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[0].empty === true) {
       $(".space1").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[0].empty = false;
+      board.spaces[0].type = "X";
+    } else if(playerO.turn === true && board.spaces[0].empty === true) {
       $(".space1").text("O");
       playerO.turn = false;
       playerX.turn = true;
+      board.spaces[0].empty = false;
+      board.spaces[0].type = "O";
     }
   });
   $(".space2").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[1].empty === true) {
       $(".space2").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[1].empty = false;
+      board.spaces[1].type = "X";
+    } else if(playerO.turn === true && board.spaces[1].empty === true) {
       $(".space2").text("O");
       playerO.turn = false;
       playerX.turn = true;
+      board.spaces[1].empty = false;
+      board.spaces[1].type = "O";
     }
   });
   $(".space3").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[2].empty === true) {
       $(".space3").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[2].empty = false;
+      board.spaces[2].type = "X";
+    } else if(playerO.turn === true && board.spaces[2].empty === true) {
       $(".space3").text("O");
       playerO.turn = false;
       playerX.turn = true;
+      board.spaces[2].empty = false;
+      board.spaces[2].type = "O";
     }
   });
   $(".space4").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[3].empty === true) {
       $(".space4").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[3].empty = false;
+      board.spaces[3].type = "X";
+    } else if(playerO.turn === true && board.spaces[3].empty === true) {
       $(".space4").text("O");
       playerO.turn = false;
       playerX.turn = true;
+      board.spaces[3].empty = false;
+      board.spaces[3].type = "O";
     }
   });
   $(".space5").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[4].empty === true) {
       $(".space5").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[4].empty = false;
+      board.spaces[4].type = "X";
+    } else if(playerO.turn === true && board.spaces[4].empty === true) {
       $(".space5").text("O");
-      playerO.turn === false;
-      playerX.turn === true;
+      playerO.turn = false;
+      playerX.turn = true;
+      board.spaces[4].empty = false;
+      board.spaces[4].type = "O";
     }
   });
   $(".space6").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[5].empty === true) {
       $(".space6").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[5].empty = false;
+      board.spaces[5].type = "X";
+    } else if(playerO.turn === true && board.spaces[5].empty === true) {
       $(".space6").text("O");
       playerO.turn = false;
       playerX.turn = true;
+      board.spaces[5].empty = false;
+      board.spaces[5].type = "O";
     }
   });
   $(".space7").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[6].empty === true) {
       $(".space7").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[6].empty = false;
+      board.spaces[6].type = "X";
+    } else if(playerO.turn === true && board.spaces[6].empty === true) {
       $(".space7").text("O");
       playerO.turn = false;
       playerX.turn = true;
+      board.spaces[6].empty = false;
+      board.spaces[6].type = "O";
     }
   });
   $(".space8").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[7].empty === true) {
       $(".space8").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[7].empty = false;
+      board.spaces[7].type = "X";
+    } else if(playerO.turn === true && board.spaces[7].empty === true) {
       $(".space8").text("O");
       playerO.turn = false;
       playerX.turn = true;
+      board.spaces[7].empty = false;
+      board.spaces[7].type = "O";
     }
   });
   $(".space9").click(function() {
-    if(playerX.turn === true) {
+    if(playerX.turn === true && board.spaces[8].empty === true) {
       $(".space9").text("X");
       playerO.turn = true;
       playerX.turn = false;
-    } else if(playerO.turn === true) {
+      board.spaces[8].empty = false;
+      board.spaces[8].type = "X";
+    } else if(playerO.turn === true && board.spaces[8].empty === true) {
       $(".space9").text("O");
       playerO.turn = false;
       playerX.turn = true;
+      board.spaces[8].empty = false;
+      board.spaces[8].type = "O";
     }
   });
 });
