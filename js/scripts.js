f288e990b688bc6d8b32c9fa9b3f1ae89517fe71
Joe Kramer and Timberlake <timberlake@>
2017-06-14 16:11:56 -0700

    mark each space, cannot repeat command on same space, update winner if three-in-a-row

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,39 @@ Board.prototype.initializeSpaces = function() {
 }
 
 Board.prototype.threeInARow = function () {
-  //determine if there are three in a row
+  if(this.spaces[0].type === "X" && this.spaces[1].type === "X" && this.spaces[2].type === "X"){
+    return true;
+  }else if(this.spaces[3].type === "X" && this.spaces[4].type === "X" && this.spaces[5].type === "X"){
+    return true;
+  }else if(this.spaces[6].type === "X" && this.spaces[7].type === "X" && this.spaces[8].type === "X"){
+    return true;
+  }else if(this.spaces[0].type === "X" && this.spaces[3].type === "X" && this.spaces[6].type === "X"){
+    return true;
+  }else if(this.spaces[1].type === "X" && this.spaces[4].type === "X" && this.spaces[7].type === "X"){
+    return true;
+  }else if(this.spaces[2].type === "X" && this.spaces[5].type === "X" && this.spaces[8].type === "X"){
+    return true;
+  }else if(this.spaces[0].type === "X" && this.spaces[4].type === "X" && this.spaces[8].type === "X"){
+    return true;
+  }else if(this.spaces[2].type === "X" && this.spaces[4].type === "X" && this.spaces[6].type === "X"){
+    return true;
+  }else if(this.spaces[0].type === "O" && this.spaces[1].type === "O" && this.spaces[2].type === "O"){
+    return true;
+  }else if(this.spaces[3].type === "O" && this.spaces[4].type === "O" && this.spaces[5].type === "O"){
+    return true;
+  }else if(this.spaces[6].type === "O" && this.spaces[7].type === "O" && this.spaces[8].type === "O"){
+    return true;
+  }else if(this.spaces[0].type === "O" && this.spaces[3].type === "O" && this.spaces[6].type === "O"){
+    return true;
+  }else if(this.spaces[1].type === "O" && this.spaces[4].type === "O" && this.spaces[7].type === "O"){
+    return true;
+  }else if(this.spaces[2].type === "O" && this.spaces[5].type === "O" && this.spaces[8].type === "O"){
+    return true;
+  }else if(this.spaces[0].type === "O" && this.spaces[4].type === "O" && this.spaces[8].type === "O"){
+    return true;
+  }else if(this.spaces[2].type === "O" && this.spaces[4].type === "O" && this.spaces[6].type === "O"){
+    return true;
+  }
 }
 
 function Game() {
@@ -43,14 +75,6 @@ $(document).ready(function(){
   board.spaces = board.initializeSpaces();
   var game = new Game();
 
-  //game loop
-  while(game.winner === false) {
-    if(playerX.turn === true) {
-
-      game.winner = board.threeInARow();
-
-    }
-  }
 
   $(".space1").click(function() {
     if(playerX.turn === true && board.spaces[0].empty === true) {
@@ -59,12 +83,21 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[0].empty = false;
       board.spaces[0].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[0].empty === true) {
       $(".space1").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[0].empty = false;
       board.spaces[0].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
   });
   $(".space2").click(function() {
@@ -74,12 +107,21 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[1].empty = false;
       board.spaces[1].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[1].empty === true) {
       $(".space2").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[1].empty = false;
       board.spaces[1].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
   });
   $(".space3").click(function() {
@@ -89,12 +131,21 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[2].empty = false;
       board.spaces[2].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[2].empty === true) {
       $(".space3").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[2].empty = false;
       board.spaces[2].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
   });
   $(".space4").click(function() {
@@ -104,12 +155,21 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[3].empty = false;
       board.spaces[3].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[3].empty === true) {
       $(".space4").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[3].empty = false;
       board.spaces[3].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
   });
   $(".space5").click(function() {
@@ -119,12 +179,21 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[4].empty = false;
       board.spaces[4].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[4].empty === true) {
       $(".space5").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[4].empty = false;
       board.spaces[4].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
   });
   $(".space6").click(function() {
@@ -134,12 +203,21 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[5].empty = false;
       board.spaces[5].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[5].empty === true) {
       $(".space6").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[5].empty = false;
       board.spaces[5].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
   });
   $(".space7").click(function() {
@@ -149,13 +227,23 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[6].empty = false;
       board.spaces[6].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[6].empty === true) {
       $(".space7").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[6].empty = false;
       board.spaces[6].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
+
   });
   $(".space8").click(function() {
     if(playerX.turn === true && board.spaces[7].empty === true) {
@@ -164,12 +252,21 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[7].empty = false;
       board.spaces[7].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[7].empty === true) {
       $(".space8").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[7].empty = false;
       board.spaces[7].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
   });
   $(".space9").click(function() {
@@ -179,12 +276,21 @@ $(document).ready(function(){
       playerX.turn = false;
       board.spaces[8].empty = false;
       board.spaces[8].type = "X";
+      $("#turnTracker").text("O's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("X Wins!");
+      }
+
     } else if(playerO.turn === true && board.spaces[8].empty === true) {
       $(".space9").text("O");
       playerO.turn = false;
       playerX.turn = true;
       board.spaces[8].empty = false;
       board.spaces[8].type = "O";
+      $("#turnTracker").text("X's Turn");
+      if(board.threeInARow()){
+        $("#turnTracker").text("O Wins!");
+      }
     }
   });
 });
